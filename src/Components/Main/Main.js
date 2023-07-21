455c1d72ba0daaaef70e43fda8f92f8c0acd8510
Andrew McLees <amclees@gmail.com>
2023-07-21 11:13:02 -0400

    start Main, Background Container, ModalStory and stylesheets

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { useHistory } from "react-router-dom";
+
+function Main() {
+  return (
+    <main>
+      {/* <ModalStory /> */}
+      {/* <BackgroundContainer /> */}
+    </main>
+  );
+}
+
+export default Main;
