5ddf548d8085e9917e317bdb2ee920175e2a5d74
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-07-21 13:20:04 -0400

    add card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import * as React from "react";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,11 +11,13 @@ import HomePage from "./pages/HomePage/HomePage";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import DisplayCardCard from "../../components/DisplayCard/DisplayCard";
 
 export default function HomePage() {
   return (
     <>
       <Header />
+      <DisplayCardCard />
 
       <Footer />
     </>
